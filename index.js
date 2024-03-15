eb5bf98ccabd10b2b183619417bafceb479dad8a
TechSiren <asinha27072002@gmailo.com>
2024-03-15 10:08:11 +0530

    worked on js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let alert = document.querySelector(".alert");
 let btn = document.querySelector(".btn");
 btn.addEventListener("click", function () {
-  alert.style.display = "block";
-  btn.disabled = true;
-  btn.style.cursor = "not-allowed";
-  
-})
+	alert.style.display = "block";
+	btn.disabled = true;
+	btn.style.cursor = "not-allowed";
+	setTimeout(() => {
+		alert.style.display = "none";
+		btn.disabled = false;
+		btn.style.cursor = "pointer";
+	}, 3000);
+});
