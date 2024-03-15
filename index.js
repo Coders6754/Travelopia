40ee115ec8a54a02f6af6cd788c05ee4ea77321b
TechSiren <asinha27072002@gmailo.com>
2024-03-15 10:07:18 +0530

    js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let alert = document.querySelector(".alert");
 let btn = document.querySelector(".btn");
 btn.addEventListener("click", function () {
+  alert.style.display = "block";
+  btn.disabled = true;
+  btn.style.cursor = "not-allowed";
   
 })
